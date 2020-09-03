c327fd4bcc1496a32bd139a25b60c6d43af2c239
Sonu kumar kushwaha <51280276+flyingsonu122@users.noreply.github.com>
2020-09-03 11:17:35 +0530

    updated

---
+++ variable.rb
@@ -0,0 +1,9 @@
+character_name = "John"
+character_age = "18"
+puts ("There once was a man named "+ character_name)
+puts ("He was " + character_age + " years old.")
+
+character_name = "Tom"
+
+puts ("He really liked the name " + character_name)
+puts ("but didn't like being " + character_age)
