a204f2d9a05a1985c5f87446817ad8f33ca7f401
Sonu kumar kushwaha <51280276+flyingsonu122@users.noreply.github.com>
2020-09-03 12:26:53 +0530

    added more files

---
+++ calculator.rb
@@ -0,0 +1,17 @@
+puts "Enter a number: "
+num1 = gets.chomp()
+
+puts "Enter another number: "
+num2 = gets.chomp()
+
+puts (num1.to_i + num2.to_i)
+
+# 
+
+puts "Enter a number: "
+num3 = gets.chomp().to_f
+
+puts "Enter another number: "
+num4 = gets.chomp().to_f
+
+puts (num3 + num4)
