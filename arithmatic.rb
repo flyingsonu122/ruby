fa5b5562ea97bbae7f64699d5a08fe046b57c083
Sonu kumar kushwaha <51280276+flyingsonu122@users.noreply.github.com>
2020-09-03 11:56:30 +0530

    added more files

---
+++ arithmatic.rb
@@ -0,0 +1,6 @@
+puts 5
+puts 51 + 10
+puts 5 - 3
+puts 5 * 3
+puts 15 / 5
+puts 15 % 2
---
+++ methods.rb
@@ -0,0 +1,14 @@
+num = 20
+puts ("My Fav num " + num.to_s)
+
+num1 = -7
+puts num1.abs()
+
+num2 = 45.5
+puts num2.round()
+
+num3 = 45.1
+puts num3.ceil()
+
+num4 = 45.9
+puts num4.floor()
